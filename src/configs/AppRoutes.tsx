6627745ea1694664ad12766400870b92986a6d5c
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2022-09-23 14:46:13 -0300

    Criando componente de tabulação sem utilizar tabela.

--- src/configs/AppRoutes.tsx
+++ src/configs/AppRoutes.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import SignIn from '../pages/SingIn'
 import Dashboard from '../pages/Dashboard'
+import Tableless from "../pages/Tablelss";
 
 export default function AppRoutes() {
     return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<SignIn />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/exerc3_tableless" element={<Tableless />} />
             </Routes>
         </Router>
     )
